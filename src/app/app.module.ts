383f77ab40d29bc737cc229be3cc9b64cc035fbd
sugamadhiakri <sugam.adhiakri123@gmail.com>
2020-05-22 16:38:27 +0545

    components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent, MenuComponent],
   imports: [
@@ -15,6 +17,9 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
