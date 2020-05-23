c886114de928659c2c36a5e3769dd522392f9100
sugamadhiakri <sugam.adhiakri123@gmail.com>
2020-05-23 18:55:27 +0545

    added dish-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, DishDetailComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
