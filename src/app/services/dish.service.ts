f952d409c8ca5ec2841dc1546531b6db15c6f678
sugamadhiakri <sugam.adhiakri123@gmail.com>
2020-06-18 14:10:38 +0545

    rxjs 3

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,15 +11,15 @@ export class DishService {
   constructor() {}
 
   getDishes(): Observable<Dish[]> {
-    return of(DISHES).pipe(delay(2000));
+    return of(DISHES).pipe(delay(500));
   }
 
   getDish(id: string): Observable<Dish> {
-    return of(DISHES.filter((dish) => dish.id === id)[0]).pipe(delay(2000));
+    return of(DISHES.filter((dish) => dish.id === id)[0]).pipe(delay(500));
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(500));
   }
 
   getDishIds(): Observable<string[] | any> {
